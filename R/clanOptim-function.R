e67c16059f813ee49a92fd6d61f7fecc53b4cf04
Claus E. Andersen <clan@dtu.dk>
2016-08-06 17:22:09 +0200

    OK

--- R/clanOptim-function.R
+++ R/clanOptim-function.R
@@ -9,7 +9,7 @@
 clanOptim <- function(){
   list(name="clanOptim",
        version=0.008,
-       date="August 3, 2014",
+       date="August 3, 2014 (uploaded August 6, 2016)",
        functions=sort(c("clanOptim",
                         "fix.parameters","optim.start","optim.fit.func","optim.predict",
                         "optim.do.simulations","optim.prediction.statistics","optim.fitting.demo"
